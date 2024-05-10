40e22a5d1cd9e5d6c035a6d06b46dd8958a788b5
JiaFengYu <JiaFengYu@github.com>
2024-05-09 20:42:52 -0400

    Trivial commit to force rebuild?

--- src/data/user.js
+++ src/data/user.js
@@ -13,7 +13,7 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Jiafeng (Jayden) Yu",
+		title: "Jayden (Jiafeng) Yu",
 		description:
 			"Hi! My name is Jiafeng but I also go by Jayden and I'm a CS and pure mathematics student at the University of Waterloo. I'm pasionate about anything from the Linux kernel to the Rust PL to real analysis and abstract algebra; it is a dream for me to eventually contribute code (ideally in Rust!) to the Linux kernel! ",
 	},
@@ -34,7 +34,8 @@ const INFO = {
 		{
 			title: "Immune System Invaders",
 			description:
-				"A twist on Space Invaders to turn it into an educational game related to the COVID-19 virus. Source code is available on my GitHub.",
+				"A twist on Space Invaders to turn it into an educational game related to the COVID-19 virus. This was 
+                create right before the virus hit Canada. Source code is available on my GitHub.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "Play the game!",
 			link: "/ISI/game.html",
